cf0d8c6533994f6014a8e97e99740c3c1bf67f30
neisantos <tecnicoeletronei@gmail.com>
2022-03-11 22:48:30 -0400

    pagamento

---
+++ Pagamento.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Pagamento {
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		Double x;
+		x = sc.nextDouble();
+		System.out.println("Voce digitou: " + x);
+		
+		sc.close();
+	}
+
+}
